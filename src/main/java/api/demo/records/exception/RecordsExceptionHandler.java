e7cc9c5374d55bd7484966f8bb3c113c208e8ff4
erikmalm <70799618+erikmalm@users.noreply.github.com>
2022-12-08 11:29:25 +0100

    Update RecordsExceptionHandler.java

--- src/main/java/api/demo/records/exception/RecordsExceptionHandler.java
+++ src/main/java/api/demo/records/exception/RecordsExceptionHandler.java
@@ -1,4 +1,19 @@
 package api.demo.records.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class RecordsExceptionHandler {
+
+    public ResponseEntity<Object> handleRecordsFieldValueMissingException
+            (FieldValueMissingException fieldValueMissingException) {
+        RecordsException recordsException = new RecordsException(
+                fieldValueMissingException.getMessage(),
+                fieldValueMissingException.getCause(),
+                HttpStatus.BAD_REQUEST
+        );
+
+        return new ResponseEntity<>(recordsException, HttpStatus.BAD_REQUEST);
+    }
+
 }
