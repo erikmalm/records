6060840af02703707d6db83d1949168d3c1be9d6
erikmalm <70799618+erikmalm@users.noreply.github.com>
2022-12-08 11:22:23 +0100

    exception handling

--- src/main/java/api/demo/records/exception/FieldValueMissingException.java
+++ src/main/java/api/demo/records/exception/FieldValueMissingException.java
@@ -1,6 +1,12 @@
 package api.demo.records.exception;
 
 public class FieldValueMissingException extends RuntimeException {
-    
 
+    public FieldValueMissingException(String message) {
+        super(message);
+    }
+
+    public FieldValueMissingException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
