519b75fd14a344436f3b36eb5abe261fc61bf167
erikmalm <70799618+erikmalm@users.noreply.github.com>
2022-12-08 11:24:27 +0100

    exception handler

--- src/main/java/api/demo/records/exception/RecordsException.java
+++ src/main/java/api/demo/records/exception/RecordsException.java
@@ -1,4 +1,30 @@
 package api.demo.records.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class RecordsException {
+
+    private final String message;
+
+    private final Throwable throwable;
+    private final HttpStatus httpStatus;
+
+    public RecordsException(String message, Throwable throwable, HttpStatus httpStatus) {
+        this.message = message;
+        this.throwable = throwable;
+        this.httpStatus = httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
 }
---
+++ src/main/java/api/demo/records/exception/RecordsExceptionHandler.java
@@ -0,0 +1,4 @@
+package api.demo.records.exception;
+
+public class RecordsExceptionHandler {
+}
