82a3e54787060ab3cb300b0ad9f768d64c6cab66
erikmalm <70799618+erikmalm@users.noreply.github.com>
2022-12-12 08:59:36 +0100

    Update EmployeeService.java

--- src/main/java/api/demo/records/service/EmployeeService.java
+++ src/main/java/api/demo/records/service/EmployeeService.java
@@ -5,12 +5,43 @@ import api.demo.records.model.Employee;
 
 import java.util.List;
 
+/**
+ * An interface for a service that manages {@link Employee} records
+ */
 
 public interface EmployeeService {
 
+    /**
+     * Adds a new employee to the records database.
+     *
+     * @param firstName the employee's first name
+     * @param lastName the employee's last name
+     * @param email the employee's email address
+     * @return the added {@link Employee}
+     * @throws RecordsFieldValueMissingException if any of the required fields (first name, last name, or email) are missing
+     */
     Employee addEmployee(String firstName, String lastName, String email) throws RecordsFieldValueMissingException;
+
+    /**
+     * Returns a list of all employees in the records database.
+     *
+     * @return a list of all employees
+     */
     List<Employee> getAllEmployees();
+
+    /**
+     * Returns the employee with the specified email address, or {@code null} if no such employee exists.
+     *
+     * @param email the email address to search for
+     * @return the employee with the specified email address, or {@code null} if no such employee exists
+     */
     Employee findEmployeeByEmail(String email);
+
+    /**
+     * Removes the employee with the specified email address from the records database.
+     *
+     * @param email the email address of the employee to remove
+     */
     void removeEmployee(String email);
 
 }
